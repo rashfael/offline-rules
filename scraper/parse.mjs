b4b91d3dca3c434fc076debfff7745f21ef6d54f
rash <sebastian.gepperth@ax-semantics.com>
2021-08-15 22:21:44 +0200

    parse magic spells

--- scraper/parse.mjs
+++ scraper/parse.mjs
@@ -10,13 +10,10 @@ const parsed = {
 }
 
 for (const page of Object.values(rawOutput)) {
-	if (page.url.includes('?')) {
-		// skip rituale and zauber for now
-		console.log('SKIPPING', page.url)
-		continue
-	}
+	// urls with ? are magic spells
+	const isSpell = page.url.includes('?')
 	if (page.breadcrumbs[0] === 'DSA Regel Wiki') page.breadcrumbs.shift()
-	console.log('PARSING', page.url, page.breadcrumbs)
+	console.log('PARSING', page.url)
 	let parent = parsed
 	while (page.breadcrumbs.length > 1) {
 		const crumb = page.breadcrumbs.shift()
@@ -29,14 +26,36 @@ for (const page of Object.values(rawOutput)) {
 		}
 		parent = parent.children[crumb]
 	}
-	const id = page.breadcrumbs.shift()
+	let id
+	// magic spells don't have their name in the breadcrumbs
+	if (!isSpell) {
+		id = page.breadcrumbs.shift()
+	}
+
+	let content = page.content
+	if (content) {
+		const $ = cheerio.load(content)
+		if ($('form').length > 0) {
+			// if we don't have an id, something went horribly wrong
+			if (!id) {
+				console.log('SKIPPING', page.url)
+				continue
+			}
+			// if we have a form, it probably is a magic filter thingy, which we can ignore
+			id = id.split('auswahl')[0]
+			content = $('.ce_text').eq(1).html()
+		} else if (isSpell) {
+			id = $('.header').text()
+		}
+	}
+
 	const parsedPage = {
 		id,
 		url: page.url,
-		content: page.content,
+		content,
 		children: parent.children[id]?.children ?? {}
 	}
 	parent.children[id] = parsedPage
 }
 
-await fs.writeFile(PARSED_OUTPUT_PATH, JSON.stringify(parsed, null, 2))
+await fs.writeFile(PARSED_OUTPUT_PATH, JSON.stringify(parsed))
